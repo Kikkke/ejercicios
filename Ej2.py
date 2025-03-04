af6fb6d7df39bde53f12e5148ea1eeb0f2aabc2b
Kikkke <lopez.garfias.enrique@gmailcom>
2025-03-03 18:38:26 -0600

    ejercicios

---
+++ Ej2.py
@@ -0,0 +1,11 @@
+num1 = int(input("Ingresa un número "))
+
+num2 = int(input("Ingresa otro numero "))
+
+suma = num2 + num1
+ 
+resta = num1 - num2
+
+print(f"La suma de los numeros es: {suma}")
+print(f"La resta de los numeros es:  {resta}")
+
